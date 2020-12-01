ed88cba726ec3e8f147ad5e899285aa3e4b71212
blend-i <55741869+blend-i@users.noreply.github.com>
2020-12-01 20:55:11 +0100

    fiksa homepage me styled components siden marius ikke vil ha chakra

--- client/src/components/Homepage/HomepageGrid.jsx
+++ client/src/components/Homepage/HomepageGrid.jsx
@@ -1,25 +1,63 @@
 import React from 'react';
-import {Grid} from '@material-ui/core';
+import styled from 'styled-components';
+
+const WholeContainer = styled.section `
+    width: 95%;
+    margin: 0 auto;
+`;
+
+const Grid = styled.section `
+    display: grid;
+    grid-template-columns: 1fr 6fr;
+    grid-template-rows: 1fr;
+    grid-gap: 30px;
+    margin: 0 auto;
+`;
+
+const BoxSection = styled.section `
+    display: flex;
+    background-color: #DBDBDB;
+    font-weight: bolder;
+    height: 300px;
+    margin-top: 30px;
+    justify-content: center;
+    align-items: center;
+
+    & h1 {
+        font-size: 30px;
+    }
+`;
+
+const BottomBox = styled.section `
+    display: flex;
+    font-weight: bolder;
+    background-color: #DBDBDB;
+    height: 400px;
+    margin-top: 30px;
+    justify-content: center;
+    align-items: center;
+
+    & h1 {
+        font-size: 30px;
+    }
+`;
 
 export const HomepageGrid = () => {
     return (
-        <Grid container spacing={6} >
-            <Grid item lg={3} sm={3} xs={4} >
-                <section style={{display:"flex", alignItems:"center", justifyContent:"center" ,height:"280px", width:"100%", background:"#DBDBDB"}}> 
-                    <p style={{fontWeight:"bolder", fontSize:"1.5em"}}>Kontorer</p>
-                </section>
+        <WholeContainer>
+            <Grid>
+                <BoxSection>
+                    <h1>Kontorer</h1>
+                </BoxSection>
+
+                <BoxSection>
+                    <h1>Kontorer</h1>
+                </BoxSection>
             </Grid>
-            <Grid item xs={7} sm={9}>
-                <section style={{display:"flex", alignItems:"center", justifyContent:"center" ,height:"280px", width:"100%", background:"#DBDBDB"}}> 
-                    <p style={{fontWeight:"bolder", fontSize:"1.5em"}}>Kontakt</p> 
-                </section>
-            </Grid>
-            <Grid item xs={12}>
-                <section style={{display:"flex", alignItems:"center", justifyContent:"center" ,height:"25rem", width:"100%", background:"#DBDBDB"}}> 
-                    <p style={{fontWeight:"bolder", fontSize:"1.5em"}}>Se våre fagartikler om oppussing av bad</p> 
-                </section>
-            </Grid>
-      </Grid>
+                <BottomBox>
+                    <h1>Kontorer</h1>
+                </BottomBox>
+        </WholeContainer>
     )
 }
 export default HomepageGrid;
