46e9b7bf7b8b1eb6e1a9b0c776b91a55d5df0aeb
mienna91 <kristomi@hiof.no>
2020-12-02 22:20:25 +0100

    merge conflicts solved

--- client/src/components/Fagartikler/ModalCategory.jsx
+++ client/src/components/Fagartikler/ModalCategory.jsx
@@ -4,85 +4,87 @@ import styled from 'styled-components';
 
 Modal.setAppElement('body');
 
-const Input = styled.input `
-    border: 1px solid black;
-    margin-bottom: 50px;
-    height: 50px;
-    width: 100%;
-    border-radius:7px;
-    font-size: 16px;
-    border-radius: 7;
+const Input = styled.input`
+  border: 1px solid black;
+  margin-bottom: 50px;
+  height: 50px;
+  width: 100%;
+  border-radius: 7px;
+  font-size: 16px;
+  border-radius: 7;
 `;
 
-const Wrapper = styled.section `
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    width: 80%;
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  width: 80%;
 `;
 
-const NewCategoryLabel = styled.label `
-    display:flex;
-    align-self: flex-start;
-    font-size: 18px;
-    font-weight: bolder;
-    margin-bottom: 5px;
-
+const NewCategoryLabel = styled.label`
+  display: flex;
+  align-self: flex-start;
+  font-size: 18px;
+  font-weight: bolder;
+  margin-bottom: 5px;
 `;
 
-const NewCategoryButtonm = styled.button `
-    display: flex;
-    flex-direction: flex-end;
-    background-color: #469fb9;
-    padding: 1.5rem 2.7rem;
-    border: 0;
-    width: 130px;
-    font-weight: bold;
-    font-size: 1em;
-    max-height: 2rem;
-    align-items: center;
-    margin-right: 1.3rem;
-    color: white;
+const NewCategoryButtonm = styled.button`
+  display: flex;
+  flex-direction: flex-end;
+  background-color: #469fb9;
+  padding: 1.5rem 2.7rem;
+  border: 0;
+  width: 130px;
+  font-weight: bold;
+  font-size: 1em;
+  max-height: 2rem;
+  align-items: center;
+  margin-right: 1.3rem;
+  color: white;
 `;
 
-
-export const ModalCategory = ({ state, close, handleCategoryChange, handleSubmit, setModalOpen }) => {
-    return (
-        state ? (<Modal
-            className="modal"
-            isOpen={state}
-            onRequestClose={setModalOpen}
-            style={
-                {
-                    overlay: {
-                        backgroundColor: 'rgba(52, 52, 52, 0.5)',
-                        width: '100%'
-                    },
-                    content: {
-                        display: 'flex',
-                        backgroundColor: '#fefefe',
-                        justifyContent: 'center',
-                        flexDirection: 'column',
-                        alignSelf: 'center',
-                        alignItems: 'center',
-                        width: '40%',
-                        height: '250px',
-                        margin: '0 auto ',
-                        marginTop: '25vh',
-                        outline: 'none',
-                    },
-                }
-            }
-        >
-        
-        <Wrapper>
+export const ModalCategory = ({
+  state,
+  close,
+  handleCategoryChange,
+  handleSubmit,
+  setModalOpen,
+}) =>
+  state ? (
+    <Modal
+      className="modal"
+      isOpen={state}
+      onRequestClose={setModalOpen}
+      style={{
+        overlay: {
+          backgroundColor: 'rgba(52, 52, 52, 0.5)',
+          width: '100%',
+        },
+        content: {
+          display: 'flex',
+          backgroundColor: '#fefefe',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          alignSelf: 'center',
+          alignItems: 'center',
+          width: '40%',
+          height: '250px',
+          margin: '0 auto ',
+          marginTop: '25vh',
+          outline: 'none',
+        },
+      }}
+    >
+      <Wrapper>
         <NewCategoryLabel>Ny Kategori</NewCategoryLabel>
-            <Input placeholder={"Placeholder for input felt"} onChange={handleCategoryChange}/>
-            <NewCategoryButtonm onClick={handleSubmit}>Create</NewCategoryButtonm>
-        </Wrapper>
-        </Modal>
-        ) : null
-    )
-};
+        <Input
+          placeholder="Placeholder for input felt"
+          onChange={handleCategoryChange}
+        />
+        <NewCategoryButtonm onClick={handleSubmit}>Create</NewCategoryButtonm>
+      </Wrapper>
+    </Modal>
+  ) : null;
 
 export default ModalCategory;
--- client/src/pages/NewArticle.jsx
+++ client/src/pages/NewArticle.jsx
@@ -74,19 +74,19 @@ const NewCategoryButton = styled.button`
   color: white;
 `;
 
-const CategoryWrapper = styled.section `
-    display:grid;
-    grid-template-columns: 12fr 1fr;
-    grid-gap: 30px;
-    margin-bottom: 50px;
+const CategoryWrapper = styled.section`
+  display: grid;
+  grid-template-columns: 12fr 1fr;
+  grid-gap: 30px;
+  margin-bottom: 50px;
 `;
 
-const AuthorWrapper = styled.section `
-    display:grid;
-    grid-template-columns: 1fr;
-    grid-gap: 30px;
-    height: 50px;
-    margin-bottom: 50px;
+const AuthorWrapper = styled.section`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 30px;
+  height: 50px;
+  margin-bottom: 50px;
 `;
 
 export const NewArticle = () => {
@@ -111,83 +111,71 @@ export const NewArticle = () => {
       ...inputValue,
     }));
   };
-  
+
   const [state, setState] = useState(false);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setState(false);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setState(false);
+  };
 
-    const handleCategoryChange = (e) => {
-        console.log(e.target.value);
-    }
+  const handleCategoryChange = (e) => {
+    console.log(e.target.value);
+  };
 
-    const handleSelect = (e) => {
-        console.log(e.target.value)
-    }
+  const handleSelect = (e) => {
+    console.log(e.target.value);
+  };
 
-    const handleAuthor = (e) => {
-        console.log(e.target.value)
-    }
+  const handleAuthor = (e) => {
+    console.log(e.target.value);
+  };
 
-    const closeModal = () => {
-        setState(false);
-    }
+  const closeModal = () => {
+    setState(false);
+  };
 
-    const [category, setCategory] = useState();
+  const [category, setCategory] = useState();
 
-    const [author, setAuthor] = useState();
+  const [author, setAuthor] = useState();
 
-        const selectCategory = () => {
-        setCategory((
-            <select onChange={handleSelect} value={category}>
-                <option>
-                    Julenisse
-                </option>
-                <option>
-                    Pepperkaker
-                </option>
-                <option>
-                    Brun Julebrus
-                </option>
-                <option>
-                    Pinnekjøtt
-                </option>
-            </select>
-        ))
-    };
+  const selectCategory = () => {
+    setCategory(
+      <select onChange={handleSelect} value={category}>
+        <option>Julenisse</option>
+        <option>Pepperkaker</option>
+        <option>Brun Julebrus</option>
+        <option>Pinnekjøtt</option>
+      </select>
+    );
+  };
 
-    const selectAuthor = () => {
-        setAuthor((
-            <select onChange={handleAuthor} value={author}>
-                <option>
-                    Iron Man
-                </option>
-                <option>
-                    Nissefar
-                </option>
-                <option>
-                    Magnus Carlsen
-                </option>
-                <option>
-                    Justin Bieber
-                </option>
-            </select>
-        ))
-    };
-    
+  const selectAuthor = () => {
+    setAuthor(
+      <select onChange={handleAuthor} value={author}>
+        <option>Iron Man</option>
+        <option>Nissefar</option>
+        <option>Magnus Carlsen</option>
+        <option>Justin Bieber</option>
+      </select>
+    );
+  };
 
-    useEffect(() => {
-        selectCategory();
-        selectAuthor();
-    }, []);
+  useEffect(() => {
+    selectCategory();
+    selectAuthor();
+  }, []);
 
   return (
     <>
       <Banner title="Ny Artikkel" />
       <InputWrapper>
-        <ModalCategory state={state} handleCategoryChange={handleCategoryChange} handleSubmit={handleSubmit} setModalOpen={closeModal}/>
+        <ModalCategory
+          state={state}
+          handleCategoryChange={handleCategoryChange}
+          handleSubmit={handleSubmit}
+          setModalOpen={closeModal}
+        />
         <Label htmlFor="title">Tittel </Label>
         <Input
           type="text"
@@ -218,14 +206,14 @@ export const NewArticle = () => {
         <Input />
         <Label>Label for kategori </Label>
         <CategoryWrapper>
-            {category}
-            <NewCategoryButton onClick={() => setState(true)}>NY</NewCategoryButton>
-            </CategoryWrapper>
+          {category}
+          <NewCategoryButton onClick={() => setState(true)}>
+            NY
+          </NewCategoryButton>
+        </CategoryWrapper>
 
-            <Label>Label for forfatter </Label>
-            <AuthorWrapper>
-                {author}
-            </AuthorWrapper>
+        <Label>Label for forfatter </Label>
+        <AuthorWrapper>{author}</AuthorWrapper>
         <NyArtikkelButton>CREATE</NyArtikkelButton>
       </InputWrapper>
     </>
