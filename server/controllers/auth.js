0f61c3e361aceb37a5fc66b2f6c8edff5740f37b
blend-i <55741869+blend-i@users.noreply.github.com>
2020-12-03 22:46:09 +0100

    fikset funksjonalitet for at man kan autorisere hvem som kan poste artikler

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -5,42 +5,51 @@ import { sendToken } from '../utils/jwtToken.js';
 
 export const register = catchAsyncErrors(async (req, res, next) => {
     const user = await userService.createUser(req.body);
-
     sendToken(user, res);
-});
-
-export const login = catchAsyncErrors(async (req, res, next) => {
-    const {email, password} = req.body;
-
-    if(!email || !password) {
-        return next(new ErrorHandler('Fyll ut epost og passord', 400))
+  });
+  
+  export const login = catchAsyncErrors(async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return next(new ErrorHandler('Fyll ut epost og passord', 400));
     }
-
-
-    const user = await userService.getUserByEmail({email}, true);
-
-
-    if(!user) {
-        return next(new ErrorHandler('Fyll ut epost og passord', 400));
+  
+    const user = await userService.getUserByEmail({ email }, true);
+  
+    if (!user) {
+      return next(new ErrorHandler('Fyll ut epost og passord', 400));
     }
-
+  
     const isPasswordMatched = await user.comparePassword(password);
-    if(!isPasswordMatched) {
-        return next(new ErrorHandler('Fyll ut epost og passord', 400)); 
+  
+    if (!isPasswordMatched) {
+      return next(new ErrorHandler('Fyll ut epost og passord', 400));
     }
-
+  
     sendToken(user, res);
-
-})
-
-export const logout = catchAsyncErrors(async (req,res,next) => {
+  });
+  
+  export const logout = catchAsyncErrors(async (req, res, next) => {
     res.cookie('token', 'none', {
-        expires: new Date(Date.now()),
-        httpOnly: true,
+      expires: new Date(Date.now()),
+      httpOnly: true,
     });
-
+  
     res.status(200).json({
-        success: true,
-        data: 'Logget ut',
-    })
-})
+      success: true,
+      data: 'Logget ut',
+    });
+  });
+
+export const currentUser = catchAsyncErrors(async (req, res, next) => {
+    const {id} = req.user;
+    const user = await userService.getUserById(id);
+    if (!user) {
+      return next(new ErrorHandler('Finner ikke brukeren', 404));
+    }
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  });
+  
--- server/services/index.js
+++ server/services/index.js
@@ -1,5 +1,5 @@
-export * as eventService from './event.js';
-export * as userService from './user.js';
 export * as articleService from './article.js';
 export * as categoryService from './category.js';
 export * as authorService from './author.js';
+export * as eventService from './event.js';
+export * as userService from './user.js';
