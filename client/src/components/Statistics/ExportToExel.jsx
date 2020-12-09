44854e9e10ae6a3bf6efb6fee8df8cd4859a116a
mienna91 <kristomi@hiof.no>
2020-12-09 17:48:36 +0100

    lint export funksjonalitet

--- client/src/components/Statistics/ExportToExel.jsx
+++ client/src/components/Statistics/ExportToExel.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
 import styled from 'styled-components';
@@ -15,25 +15,26 @@ const NewCategoryButton = styled.button`
 
 /**
  * Brukt exempel fra denne siden for export til exel: https://technicaaadda.blogspot.com/2020/11/export-data-to-excel-using-react.html
- * @param {*} param0 
+ * @param {*} param0
  */
 
-export const ExportToExel = ({csvData, fileName}) => {
+export const ExportToExel = ({ csvData, fileName }) => {
+  const fileType =
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
 
-    const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+  const fileExtension = '.xlsx';
 
-    const fileExtension = '.xlsx';
+  const exportToCSV = (csvData, fileName) => {
+    const ws = XLSX.utils.json_to_sheet(csvData);
+    const wb = { Sheets: { data: ws }, SheetNames: ['data'] };
+    const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    const data = new Blob([excelBuffer], { type: fileType });
+    FileSaver.saveAs(data, fileName + fileExtension);
+  };
 
-    const exportToCSV = (csvData, fileName) => {
-        const ws = XLSX.utils.json_to_sheet(csvData);
-        const wb = { Sheets: { 'data': ws }, SheetNames: ['data'] };
-        const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
-        const data = new Blob([excelBuffer], {type: fileType});
-        FileSaver.saveAs(data, fileName + fileExtension);
-    }
-
-    return (
-        <NewCategoryButton  onClick={(e) => exportToCSV(csvData,fileName)}>Export</NewCategoryButton>
-    )
+  return (
+    <NewCategoryButton onClick={(e) => exportToCSV(csvData, fileName)}>
+      Export
+    </NewCategoryButton>
+  );
 };
-
